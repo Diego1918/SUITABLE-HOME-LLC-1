a5cf7387a8277bb6f14315f48857740bb764e9b6
Jolmev <jolmev@hotmail.com>
2022-10-02 22:54:52 -0500

    BACKEND

---
+++ SuitableHome2/src/main/java/com/app/web/interfaz/PropiedadesInterfazImpl.java
@@ -0,0 +1,42 @@
+package com.app.web.interfaz;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.web.entidad.Propiedades;
+import com.app.web.repositorio.PropiedadesRepositorio;
+
+@Service
+public class PropiedadesInterfazImpl implements PropiedadesInterfaz {
+
+	@Autowired
+	private PropiedadesRepositorio repositorio;
+	
+	@Override
+	public List<Propiedades> listarTodasLasPropiedades() {
+		//System.out.println(repositorio.findAll());
+		return repositorio.findAll();
+	}
+
+	@Override
+	public Propiedades guardarPropiedad(Propiedades propiedades) {
+		return repositorio.save(propiedades);
+	}
+
+	@Override
+	public Propiedades obtenerPropiedadPorId(Long id) {
+		return repositorio.findById(id).get();
+	}
+
+	@Override
+	public Propiedades actualizarPropiedad(Propiedades propiedades) {
+		return repositorio.save(propiedades);
+	}
+
+	@Override
+	public void eliminarPropiedad(Long id) {
+		repositorio.deleteById(id);
+	}
+}
